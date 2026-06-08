f72980d560ac1875629dd6c0238c00192268c47c
khizer-25 <khizerbaig172@gmail.com>
2026-06-08 20:25:26 +0530

    Backend ready for deployment

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/perfume-db');
+    const conn = await mongoose.connect(process.env.MONGO_URI );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Database Connection Error: ${error.message}`);
